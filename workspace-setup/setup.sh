b16c1c19f1207daad940974ab1bd540281fa4e5b
alibektas <bektasali@protonmail.com>
2023-02-26 03:27:55 +0100

    Add -y arg to skip apt-get's questions.

--- workspace-setup/setup.sh
+++ workspace-setup/setup.sh
@@ -1,7 +1,11 @@
-# This script installs some programs that I like using.
+#This script installs some programs that I like using.
 
 apt-get update
 apt-get install -y neovim exa
-apt-get upgrade neovim
+apt-get upgrade -y neovim
+
+# Install plug.vim neovim/vim package manager.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.c    om/junegunn/vim-plug/master/plug.vim'                                           
+
+# Replace ls with exa. 
 echo 'alias ls=exa -T -a --level 2 --icons' >> ~/.shrc
