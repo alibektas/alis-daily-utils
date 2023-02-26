2d8bdd2dbf3f2d9cb957f663003a6ac52ed27bd6
alibektas <bektasali@protonmail.com>
2023-02-26 14:02:45 +0100

    Amend links to vim.plug

--- workspace-setup/setup.sh
+++ workspace-setup/setup.sh
@@ -1,11 +1,12 @@
-#This script installs some programs that I like using.
+#This script installs some programs I like using.
 
 apt-get update
 apt-get install -y neovim exa
 apt-get upgrade -y neovim
 
 # Install plug.vim neovim/vim package manager.
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.c    om/junegunn/vim-plug/master/plug.vim'                                           
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'                                           
 
 # Replace ls with exa. 
 echo 'alias ls=exa -T -a --level 2 --icons' >> ~/.shrc
+cp ./init.vim ~/.config/nvim/init.vim
