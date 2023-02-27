0fc45778658b7404719aee1d2a1689b527a4e856
alibektas <bektasali@protonmail.com>
2023-02-27 01:54:50 +0100

    Experimenting with stuff.

--- workspace-setup/setup.sh
+++ workspace-setup/setup.sh
@@ -1,3 +1,4 @@
+# ABC
 #This script installs some programs I like using.
 apt-get update
 apt-get install -y neovim exa
@@ -7,7 +8,9 @@ apt-get upgrade -y neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'                                           
 
 # Replace ls with exa. 
-echo 'alias ls=exa -T -a --level 2 --icons' >> ~/.shrc
-mkdir ~/.config/nvim
-cp -r ./init.vim ~/.config/nvim/init.vim
+echo 'alias ls=exa -T -a --level 2 --icons' >> ~/.bashrc
+mkdir -p ~/.config/nvim
+ln -s ./init.vim ~/.config/nvim/init.vim
 nvim +PlugInstall +qall
+
+
