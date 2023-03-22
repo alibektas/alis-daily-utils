281f29e5db1ad38bef00ab4804987c81b9a37e40
alibektas <bektasali@protonmail.com>
2023-03-22 14:47:44 +0100

    Remove Neovim configs
    We are migrating to Helix editor cuz it is easier to use and we love things developed in Rust

--- workspace-setup/setup.sh
+++ workspace-setup/setup.sh
@@ -1,17 +1,11 @@
-# DEF
-#This script installs some programs I like using.
 apt-get update
-apt-get install -y neovim exa
-apt-get upgrade -y neovim
 
-# Install plug.vim neovim/vim package manager.
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'                                           
+# Coc requires node
+apt-get install node.js
+apt-get install -y helix exa
+
 
 # Replace ls with exa. 
-echo 'alias ls="exa -T -a --level 1 --icons"' >> ~/.bashrc
-echo 'alias vim="nvim -u /alis-daily-utils/workspace-setup/init.vim"' >> ~/.bashrc
 source ~/.bashrc
 
-nvim +PlugInstall +qall --headless
-nvim +'CocInstall coc-tsserver' +qall --headless
 
